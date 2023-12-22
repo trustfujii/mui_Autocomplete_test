808bc5d76e389b22184b62b01586193b14f68283
CodeSandbox <support@codesandbox.io>
2023-12-22 08:10:43 +0000

    Sandbox snapshot commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import "./styles.css";
+import React from "react";
+import { useState } from "react";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
 
 export default function App() {
+  const [value, setValue] = useState();
+  const options = [
+    { id: 1, name: "Ken", role: 0, age: 35 },
+    { id: 2, name: "Taro", role: 1, age: 42 },
+    { id: 3, name: "Tom", role: 2, age: 45 },
+  ];
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Autocomplete
+        size="small"
+        sx={{ width: "300px" }}
+        options={options}
+        isOptionEqualToValue={(option, v) => option.id === v.id}
+        getOptionLabel={(option) => option.name}
+        onChange={(_e, item) => {
+          console.log(item)
+          return setValue(item);
+        }}
+        value={value || null}
+        noOptionsText="見つかりません"
+        renderOption={(props, option) => {
+          return (
+            <Box component="li" {...props} key={option.id}>
+              {option.name}
+            </Box>
+          );
+        }}
+        renderInput={(params) => <TextField {...params} fullWidth />}
+      />
     </div>
   );
 }
